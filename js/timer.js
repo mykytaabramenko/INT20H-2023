e6ea09f5c46c8bae0583ba66540f1475f00b29ad
Mykyta Abramenko <mykytaabramenko@gmail.com>
2023-01-02 10:58:44 +0200

    timer fix

--- js/timer.js
+++ js/timer.js
@@ -1,36 +1,31 @@
 const countDownDate = new Date("March 5, 2023 11:00:00").getTime();
 
-// Update the count down every 1 second
 let x = setInterval(function() {
+  var now = new Date().getTime();
 
-  // Get today's date and time
-    var now = new Date().getTime();
+  var distance = countDownDate - now;
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  0 <= days && days < 10? document.getElementById('days').innerHTML = 
+  "0" + days : document.getElementById('days').innerHTML = days;
+  
+  0 <= hours && hours < 10? document.getElementById('hours').innerHTML =
+  "0" + hours : document.getElementById('hours').innerHTML = hours;
+  
+  0 <= minutes && minutes < 10? document.getElementById('minutes').innerHTML = 
+  "0" + minutes : document.getElementById('minutes').innerHTML = minutes;
 
-  // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Display the result in the element with id="demo"
-    document.getElementById('days').innerHTML = days;
-    document.getElementById('hours').innerHTML = hours;
-    document.getElementById('minutes').innerHTML = minutes;
-    document.getElementById('seconds').innerHTML = seconds;
-    /*
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-    */
-    // If the count down is finished, write some text
-    
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById('days').innerHTML = 0;
-        document.getElementById('hours').innerHTML = 0;
-        document.getElementById('minutes').innerHTML = 0;
-        document.getElementById('seconds').innerHTML = 0;
-    }
+  0 <= seconds && seconds < 10? document.getElementById('seconds').innerHTML = 
+  "0" + seconds : document.getElementById('seconds').innerHTML = seconds; 
+  
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById('days').innerHTML = "00";
+    document.getElementById('hours').innerHTML = "00";
+    document.getElementById('minutes').innerHTML = "00";
+    document.getElementById('seconds').innerHTML = "00";
+  }
 }, 1000);
